62cca56277220e06adef30a9ab22224eb6b62759
wgdp444 <tatsuyori.shiraga@triple-four.com>
2021-10-17 15:31:18 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
+use std::io::stdin;
+use std::io::stdout;
+use std::io::Write;
+
+extern crate regex;
+
+use regex::Regex;
+
 mod ascii;
 
 
-struct MainMenu {
-    title: String
+struct Title {
+    text: String
 }
-fn draw_menu(menu: &MainMenu) {
-   println!("{}", menu.title);
+fn draw_title(title: &Title) {
+   println!("{}", title.text);
+}
+
+fn validate (text: String) -> bool {
+    let re = Regex::new(r"^\d{4}$").unwrap();
+    let new_text = &text.trim();
+    re.is_match(new_text)
+}
+
+fn update() {
+    print!("Hit and blowは、ランダムに生成された4桁の数字を当てるゲームです。\n数字を入力してください＞");
+    stdout().flush().unwrap();
+    let mut input_num = String::new();
+    stdin().read_line(&mut input_num).expect("失敗");
+    println!("{}", input_num);
+    if !validate(input_num) {
+    }
+
 }
 fn main() {
-    let menu = MainMenu {
-        title: ascii::TITLE.to_string()
+    let title = Title {
+        text: ascii::TITLE.to_string()
     };
-    draw_menu(&menu);
+    draw_title(&title);
+    
+    loop {
+        update();
+    }
+    
 }
