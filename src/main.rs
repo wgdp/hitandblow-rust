620cba5a49868ac4389a7046b22f2d798a64cbcb
wgdp <tatsuyori4649@gmail.com>
2021-12-30 23:09:04 +0900

    update: rand

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,49 @@ use std::io::stdin;
 use std::io::stdout;
 use std::io::Write;
 
+extern crate rand;
 extern crate regex;
 
 use regex::Regex;
 
 mod ascii;
 
-
 struct Title {
-    text: String
+    text: String,
 }
 fn draw_title(title: &Title) {
-   println!("{}", title.text);
+    println!("{}", title.text);
 }
 
-fn validate (text: String) -> bool {
+fn validate(text: String) -> bool {
     let re = Regex::new(r"^\d{4}$").unwrap();
     let new_text = &text.trim();
     re.is_match(new_text)
 }
 
-fn update() {
+fn update() -> bool {
     print!("Hit and blowは、ランダムに生成された4桁の数字を当てるゲームです。\n数字を入力してください＞");
     stdout().flush().unwrap();
     let mut input_num = String::new();
     stdin().read_line(&mut input_num).expect("失敗");
     println!("{}", input_num);
+
+    // 数値がおかしい場合continue
     if !validate(input_num) {
+        println!("4桁の整数を入力してください。");
+        return false;
     }
 
+    return true;
 }
+
 fn main() {
     let title = Title {
-        text: ascii::TITLE.to_string()
+        text: ascii::TITLE.to_string(),
     };
     draw_title(&title);
-    
+
     loop {
         update();
     }
-    
 }
